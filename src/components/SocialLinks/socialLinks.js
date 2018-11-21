17b06470c75014cb492b02f8f27b0d489d055f6f
patrickbjohnson <pbj@progresslabs.co>
2018-11-20 21:55:19 -0500

    build social links section

--- src/components/SocialLinks/socialLinks.js
+++ src/components/SocialLinks/socialLinks.js
@@ -5,7 +5,14 @@ import styles from './socialLinks.module.css'
 const SocialLinks = () => {
     return (
         <section className={styles.section}>
-            <h1>Social Links</h1>
+            <div className={styles.container}>
+                <a className={styles.link} href="#" target="_blank" rel="noopener noreferrer">Instagram</a>
+                <a className={styles.link} href="#" target="_blank" rel="noopener noreferrer">Medium</a>
+                <a className={styles.link} href="#" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a className={styles.link} href="#" target="_blank" rel="noopener noreferrer">Spotify</a>
+                <a className={styles.link} href="#" target="_blank" rel="noopener noreferrer">Twitter</a>            
+            </div>
+            <a href="https://goo.gl/maps/HLm4KDK4AS22" className={styles.address} target="_blank" rel="noopener noreferrer">2745, 19th Street, San Francisco, California, 94110</a>
         </section>
     )
 }
