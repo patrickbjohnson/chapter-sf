249422545933167577ae80e75fa7b088c009b81f
patrickbjohnson <pbj@progresslabs.co>
2018-12-01 15:57:55 -0500

    add hover events for desktop client panel

--- src/components/Client/client.js
+++ src/components/Client/client.js
@@ -5,8 +5,10 @@ import styles from './client.module.css'
 class Client extends Component {
     constructor(props) {
         super(props)
+
+        this.clientRef = React.createRef();
         this.state = {
-            isVisible: false,
+            active: null,
             hasInfo: false
         }
     }
@@ -17,29 +19,40 @@ class Client extends Component {
         })
     }
 
-    toggleInfoDisplay = () => {
-        if (!this.state.hasInfo) return
-        this.setState({
-            isVisible: !this.state.isVisible
-        })
+    panelClasses = () => {
+        return this.props.isActive ? `${styles.panel} ${styles.active}` : styles.panel
     }
 
-
     render () {
-        const { name, info } = this.props
+        
+        const { 
+            name, 
+            info,
+            eventHandler,
+            isActive,
+            hideEvent,
+            mq
+        } = this.props
+
         return (
-            <div onClick={this.toggleInfoDisplay} className={styles.client}>
+            <div 
+                ref={this.clientRef}
+                className={styles.client}   
+                onClick={() => {
+                    if (mq && this.state.hasInfo) return false
+                    eventHandler()
+                }}
+
+                onMouseEnter={() => {
+                    if (!mq && this.state.hasInfo) return false
+                    eventHandler()
+                }}
+            >
                 {name}
                 {info && 
-                    <>
-                        <span>*
-                        {this.state.isVisible && 
-                            <div className={styles.panel}>{info}</div>
-                        }
-                        </span>
-
-                        
-                    </>
+                    <span>*
+                        <div className={this.panelClasses()}>{info}</div>
+                    </span>
                 }
             </div>
         )
--- src/components/Clients/clients.js
+++ src/components/Clients/clients.js
@@ -1,72 +1,142 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Client from '../Client'
 
 import styles from './clients.module.css'
 
-const Clients = () => {
-    const clients = [
-        {
-            name: 'Google',
-            info: false
-        },
-        {
-            name: 'Airbnb',
-            info: 'How might we make a more engaging and helpful experience for the financially underserved?'
-        },
-        {
-            name: 'Sonos',
-            info: false
-        },
-        {
-            name: 'Dropbox',
-            info: false
-        },
-        {
-            name: 'Oculus',
-            info: false
-        },
-        {
-            name: 'Uber',
-            info: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.'
-        },
-        {
-            name: 'Domo',
-            info: false
-        },
-        {
-            name: 'Kodak',
-            info: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.'
-        },
-        {
-            name: 'Facebook',
-            info: false
-        },
-        {
-            name: 'PayPal',
-            info: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.'
-        },
-        {
-            name: 'Target',
-            info: false
-        },
-        {
-            name: 'Pillpack',
-            info: false
-        }
-    ]
-    return (
-        <section className={styles.section}>
-            <h2 className={styles.title}>Some of our pioneering partners</h2>
+class Clients extends Component {
+    constructor() {
+        super()
 
-            <ul className={styles.clientList}>
-                {clients.map((client, i) => {
-                        return <li key={i}><Client {...client}/></li>
-                    })
+        this.state = {
+            mq: null,
+            activeClient: null,
+            clients: [
+                {
+                    name: 'Google',
+                    info: false
+                },
+                {
+                    name: 'Airbnb',
+                    info: 'How might we make a more engaging and helpful experience for the financially underserved?'
+                },
+                {
+                    name: 'Sonos',
+                    info: false
+                },
+                {
+                    name: 'Dropbox',
+                    info: false
+                },
+                {
+                    name: 'Oculus',
+                    info: false
+                },
+                {
+                    name: 'Uber',
+                    info: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.'
+                },
+                {
+                    name: 'Domo',
+                    info: false
+                },
+                {
+                    name: 'Kodak',
+                    info: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.'
+                },
+                {
+                    name: 'Facebook',
+                    info: false
+                },
+                {
+                    name: 'PayPal',
+                    info: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.'
+                },
+                {
+                    name: 'Target',
+                    info: false
+                },
+                {
+                    name: 'Pillpack',
+                    info: false
                 }
-            </ul>
-        </section>
-    )
+            ]
+        }
+    }
+
+    componentDidMount() {
+        const mq = window.matchMedia('(min-width: 1024px)')
+        this.setState({
+            mq: mq.matches
+        })
+        
+        mq.addListener((data) => {
+            this.setState({
+                mq: data.matches
+            })
+        })
+
+        this.state.clients.map((client, i) => {
+            const dom = this.refs[client.name].clientRef.current;
+            console.log(client)
+            dom.addEventListener('mouseleave', (e) => {
+                if ((this.state.activeClient !== client.name) || (!client.info )) return false;
+                const currentClient = this.state.activeClient
+                
+                setTimeout(() => {
+                    if (currentClient == this.state.activeClient) {
+                        this.setState({
+                            activeClient: null
+                        })
+                        clearTimeout()
+                    } else {
+                        clearTimeout()
+                    }
+                }, 800)
+            })
+            
+        })
+    }
+
+    toggleInfoDisplay = (clientName) => {
+        this.setState({
+            activeClient: clientName
+        })
+    }
+
+    hideInfoDisplay = () => {
+        this.setState({
+            activeClient: null
+        })
+    }
+
+    render() {
+        return (
+            <section className={styles.section}>
+                <h2 className={styles.title}>Some of our pioneering partners</h2>
+    
+                <ul className={styles.clientList}>
+                    {this.state.clients.map((client, i) => {
+                            return (
+                                <li key={i}>
+                                    <Client 
+                                        ref={client.name} 
+                                        isActive={this.state.activeClient === client.name}
+                                        hideEvent={this.hideInfoDisplay}
+                                        eventHandler={() => {
+                                            this.toggleInfoDisplay(client.name)
+                                        }}
+                                        mq={this.state.mq}
+                                        {...client} 
+                                    />
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </section>
+        )
+    }
 }
 
 export default Clients
