4ecb656429b442aa3c70411892892f0062390556
patrickbjohnson <pbj@progresslabs.co>
2018-12-23 22:47:39 -0500

    update colors and hover states

--- src/components/Meet/meet.js
+++ src/components/Meet/meet.js
@@ -14,7 +14,7 @@ const Meet = () => {
             >
                 <Content>
                     <h2
-                        className={content.title}
+                        className={`${content.title} ${content.whiteFill}`}
                         data-sal="slide-up"
                         data-sal-delay="100"
                         data-sal-duration="600"
@@ -23,7 +23,7 @@ const Meet = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 561.7 116"><path d="M7.8 5.3h24.4L68.6 89l40.5-83.2h22.5v104.7h-16.1l.8-88.3L105 46.9l-31 63.6H62L33.9 47.7 23 22.2l.9 88.3H7.8V5.3zM181.3 5.2h89.1v14.6h-72.9v23.9h61.2v14.6h-61.4.1v38.1H274v14.4h-92.7V5.2zM319.4 5.2h89.1v14.6h-72.9v23.9h61.2v14.6h-61.4.1v38.1H412v14.4h-92.7V5.2zM449.5 5.2h106.1v14.4h-44.9v91.2h-16.1V19.7h-45.1V5.2z"/></svg>
                     </h2>
                     <p
-                        className={content.copy}
+                        className={`${content.copy} ${content.white}`}
                         data-sal="slide-up"
                         data-sal-delay="100"
                         data-sal-duration="600"
