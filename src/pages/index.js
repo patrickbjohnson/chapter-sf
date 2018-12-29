eb1bf1bcaec92de184fc8a69e6d80192a85b2cb2
patrickbjohnson <pbj@progresslabs.co>
2018-12-29 15:23:50 -0500

    clean up components

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ import Menu from '../components/DesktopMenu'
 import Partner from '../components/Partner'
 import Clients from '../components/Clients'
 import Projects from '../components/Projects'
-import CTARow from '../components/CTARow'
 import Tumblr from '../components/Tumblr'
 import NewKind from '../components/NewKind'
 
