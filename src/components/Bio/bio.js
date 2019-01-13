e06aa7b85588af804d70f1b5de3d6cd961afabf0
patrickbjohnson <pbj@progresslabs.co>
2019-01-13 14:38:02 -0500

    update about to include bios

---
+++ src/components/Bio/bio.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './bio.module.css'
+
+const Bio = (props) => {
+    return (
+        <div className={styles.bioWrap}>
+            <div
+                    className={styles.bio}
+                    style={{ backgroundImage: `url(${props.image})` }}
+            >
+                <div className={styles.bioCopy}>
+                  <h3 className={styles.name}>{props.name}</h3>
+                  <span className={styles.title}>{props.title}</span>
+                  <p className={styles.desc}>{props.desc}</p>
+                </div>
+            </div>
+            <a className={styles.bioLink} href="#">LinkedIn</a>
+            <a className={styles.bioLink} href="#">Twitter</a>
+        </div>
+
+    )
+}
+
+export default Bio
--- src/components/Us/us.js
+++ src/components/Us/us.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Content from '../Content'
+import Bio from '../Bio'
 import content from '../Content/content.module.css'
 import styles from './us.module.css'
 
@@ -16,37 +17,64 @@ const Us = () => {
           data-sal-easing="ease-out-bounce"
           >
           <p className={`${styles.copy} ${content.copySmall}`}>
-            This is Chapter. We do things differently here. Founded by{' '}
-            <a
-              href="https://www.linkedin.com/in/neilrobinson/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Gareth Kay
-            </a>{' '}
-            and{' '}
-            <a
-              href="https://www.linkedin.com/in/garethkaysf/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Neil Robinson
-            </a>{' '}
-            in 2014, we’re a group of optimistic misfits whose past lives spans
-            time in creative places as well as time in the classroom, the pub
-            and the shed—meet us and figure out which of us was an English
-            teacher, who was the barmaid and who is the woodworker.
-          </p>
-          <p className={`${styles.copy} ${content.copySmall}`}>
-            We’re a new type of creative studio, built from the ground up to
-            better deliver the commercial creativity pioneering companies need
-            today. Creativity that is defined by the impact it has, not the
-            channel it is delivered in. Creativity that closes the growing gap
-            between what people expect and what companies deliver.
+            WE’RE BUILDING A HOME FOR BRILLIANT MISFITS
           </p>
         </div>
       </Content>
-
+      <div className={styles.grid}>
+        <div className={styles.col}
+          data-sal="slide-up"
+          data-sal-delay="100"
+          data-sal-duration="600"
+          data-sal-easing="ease-out-bounce"
+        >
+          <Bio
+            image="http://placehold.it/300x400"
+            name="Gareth Kay"
+            title="Co-Founder"
+            desc="Ex-CSO of Goodby, Silverstein and Partners. Left a 20 year career at some of the world’s best ad agencies to apply strategic creativity more expansively. Failed musician."
+          />
+        </div>
+        <div className={styles.col}
+          data-sal="slide-up"
+          data-sal-delay="200"
+          data-sal-duration="600"
+          data-sal-easing="ease-out-bounce"
+        >
+          <Bio
+            image="http://placehold.it/300x400"
+            name="Neil Robinson"
+            title="Co-Founder"
+            desc="Ex-ECD AQKA San Francisco. After helping to build one of the best agencies in the world for 17 years hit the reset button to build something from the ground up that's designed to do the right thing."
+          />
+        </div>
+        <div className={styles.col}
+          data-sal="slide-up"
+          data-sal-delay="300"
+          data-sal-duration="600"
+          data-sal-easing="ease-out-bounce"
+        >
+          <Bio
+            image="http://placehold.it/300x400"
+            name="Sue Murphy"
+            title="Partner"
+            desc="Ex-Wieden+Kennedy, Wolff Olins and Ogilvy. The odd-shaped maverick swapping sides of the fence in advertising, branding and design studios. Pulling pints since age 6. Designing since age 11."
+          />
+        </div>
+      </div>
+      <Content>
+        <div
+          className={`${styles.contentInner} ${styles.bottom}`}
+          data-sal="slide-up"
+          data-sal-delay="100"
+          data-sal-duration="600"
+          data-sal-easing="ease-out-bounce"
+        >
+          <p className={`${styles.copy} ${content.copySmall}`}>
+          Our experience has led us to do things a bit differently. We act as an agent of change not an agency of record. We’re obsessed in finding the human opportunity or problem and solving or seizing it in whatever shape it takes. We break down silos. We move fast. We’re open. We show the thing. We’re fearless, optimistic explorers in search of the better path less traveled.
+          </p>
+        </div>
+      </Content>
     </section>
   )
 }
