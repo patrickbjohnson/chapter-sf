da0c08a2e9433183af6ba5f9408e5ce19b99a20d
patrickbjohnson <pbj@progresslabs.co>
2019-01-13 16:08:05 -0500

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,38 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+      logo: "./static/favicon.png",
+
+      // WebApp Manifest Configuration
+      appName: null, // Inferred with your package.json
+      appDescription: null,
+      developerName: null,
+      developerURL: null,
+      dir: 'auto',
+      lang: 'en-US',
+      background: '#fff',
+      theme_color: '#fff',
+      display: 'standalone',
+      orientation: 'any',
+      start_url: '/?homescreen=1',
+      version: '1.0',
+
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: true,
+        twitter: true,
+        yandex: false,
+        windows: true
+      }
+      }
+    }
   ],
 }
